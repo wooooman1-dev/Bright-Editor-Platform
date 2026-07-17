44ca7bf886718ad4af262f7c3918aa9488035612
wooooman1-dev <wooooman1@gmail.com>
2026-07-17 20:16:03 +0900

    feat: export presentation contracts

---
+++ core/presentation/index.ts
@@ -0,0 +1,34 @@
+export {
+  brightSemanticRoles,
+  platformIds,
+  semanticFallbackElements,
+  type BrightSemanticRole,
+  type ComponentFallbackPolicy,
+  type ComponentPresentationNode,
+  type PlatformId,
+  type PresentationDocument,
+  type PresentationNode,
+  type PresentationWarning,
+  type PresentationWarningSeverity,
+  type SemanticFallbackElement,
+  type SemanticFallbackNode,
+  type ThemeReference,
+} from "./PresentationModel";
+export type { PresentationError, PresentationErrorCategory } from "./PresentationError";
+export { presentationErrorCategories } from "./PresentationError";
+export type {
+  PresentationResolutionOptions,
+  PresentationResolutionRequest,
+  UnsupportedComponentPolicy,
+} from "./PresentationRequest";
+export type {
+  PresentationCompatibilityResult,
+  PresentationCompatibilityStatus,
+  PresentationVersions,
+} from "./PresentationVersion";
+export {
+  validatePresentationDocument,
+  type PresentationValidationIssue,
+  type PresentationValidationIssueCode,
+  type PresentationValidationResult,
+} from "./PresentationValidator";
