88a6a4deaa149f681d12efc1ca5aaa9c6e0dc319
wooooman1-dev <wooooman1@gmail.com>
2026-08-03 13:54:10 +0900

    refactor: export universal source adapter

--- core/approval/index.ts
+++ core/approval/index.ts
@@ -1,6 +1,7 @@
 export * from "./ApprovalPolicy";
 export * from "./ApprovalReadiness";
 export * from "./ApprovalDuplicatePolicy";
+export * from "./ApprovalSourceDocumentAdapter";
 export type {
   ApprovalEvidenceVerificationResult,
   ApprovalSourcePage,
